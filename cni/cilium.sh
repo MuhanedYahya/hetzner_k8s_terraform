0f18a3ef0bbfc9a0838907feec02b545a9e7b24e
MuhanedYahya <yahya.muhaned@gmail.com>
2024-07-28 01:16:06 +0300

    tfvars configured

--- cni/cilium.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-helm repo add cilium https://helm.cilium.io/
-helm install cilium cilium/cilium --version 1.15.5 --namespace kube-system
--- cni/flannel.sh
+++ cni/flannel.sh
@@ -3,4 +3,4 @@ kubectl create ns kube-flannel
 kubectl label --overwrite ns kube-flannel pod-security.kubernetes.io/enforce=privileged
 helm repo add flannel https://flannel-io.github.io/flannel/
 # podCidr must match k8s's --pod-network-cidr
-helm install flannel --set podCidr="10.244.0.0/16" --namespace kube-flannel flannel/flannel
+helm install flannel --set podCidr="192.168.0.0/16" --namespace kube-flannel flannel/flannel
